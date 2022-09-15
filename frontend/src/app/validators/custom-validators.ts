8a58c67d7e987b6ef2f2fec107b119e30a167093
raiDen100 <34070652+raiDen100@users.noreply.github.com>
2022-09-15 18:25:19 +0200

    Add files via upload

---
+++ frontend/src/app/validators/custom-validators.ts
@@ -0,0 +1,9 @@
+import { FormControl, ValidationErrors } from "@angular/forms";
+
+export class CustomValidators {
+    static notOnlyWhitespace(control: FormControl): ValidationErrors{
+        if((control.value != null) && (control.value.trim().length === 0))
+            return {'notOnlyWhitespaces': true};
+        return {};
+    }
+}
